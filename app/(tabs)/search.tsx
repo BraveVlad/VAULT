ac41836891d35a408dd16f9a03ccba08ae078f75
BraveVlad <giladvladpinker@gmail.com>
2024-05-26 02:04:15 +0300

    reverted env variable usage due to expo not recognizing main index after metro loads env variables

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,7 +1,23 @@
 import { mainStyles } from "@/constants/Styles";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+function fetchGames() {
+	// const key = Constants.manifest2?.extra.rawgApiKey;
+}
+
 export default function SearchScreen() {
+	const [gamesList, setGamesList] = useState();
+	// TODO - FETCH RAWG API
+
+	useEffect(() => {
+		//mount
+		fetchGames();
+
+		return () => {
+			//unmount
+		};
+	}, []);
 	return (
 		<View style={mainStyles.Screen}>
 			<Text style={mainStyles.Text}>SEARCH</Text>
