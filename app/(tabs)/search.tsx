66f3af6e8d3e26d875dc5cbeb7955c0efa453a9b
BraveVlad <giladvladpinker@gmail.com>
2024-05-26 03:25:15 +0300

    + Search page now fetches games and maps a basic view. Next step is FlatList.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,18 +1,32 @@
+import { RAWG_API_KEY, getGamesListRequest } from "@/constants/Api";
 import { mainStyles } from "@/constants/Styles";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import axios from "axios";
+import { ApiResponse as GamesResponse, Game } from "@/models/Game.Model";
 
-function fetchGames() {
-	// const key = Constants.manifest2?.extra.rawgApiKey;
+async function fetchGames() {
+	const request = getGamesListRequest(10);
+
+	return axios
+		.get(request)
+		.then(async (response) => {
+			const gamesData = response.data;
+			console.log(gamesData);
+			return gamesData;
+		})
+		.catch((error) => {
+			console.error(error);
+			return {} as GamesResponse;
+		});
 }
 
 export default function SearchScreen() {
-	const [gamesList, setGamesList] = useState();
-	// TODO - FETCH RAWG API
+	const [gamesList, setGamesList] = useState<GamesResponse>();
 
 	useEffect(() => {
 		//mount
-		fetchGames();
+		fetchGames().then(setGamesList);
 
 		return () => {
 			//unmount
@@ -20,7 +34,17 @@ export default function SearchScreen() {
 	}, []);
 	return (
 		<View style={mainStyles.Screen}>
-			<Text style={mainStyles.Text}>SEARCH</Text>
+			<Text style={mainStyles.Text}>Total Games: {gamesList?.count}</Text>
+
+			{gamesList?.results.map((game) => (
+				<View key={game.id}>
+					<Image
+						source={{ uri: game.background_image }}
+						style={{ width: 32, height: 32 }}
+					/>
+					<Text style={mainStyles.Text}>{game.name}</Text>
+				</View>
+			))}
 		</View>
 	);
 }
