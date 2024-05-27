9ceea288780c2ecc91defb4e11a623ae2c69c2bb
BraveVlad <giladvladpinker@gmail.com>
2024-05-27 23:14:25 +0300

    front:SingleGameScreen: implemented react query to handle single game fetching

--- vault/app/[gameId].tsx
+++ vault/app/[gameId].tsx
@@ -4,36 +4,50 @@ import { useLocalSearchParams } from "expo-router";
 import { getMockGameRequest, getMockGamesListRequest } from "@/constants/Api";
 import axios from "axios";
 import { Game } from "@/models/Game.Model";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import GameResultView from "@/components/search-screen/GameResultView";
+import { colors } from "@/constants/Colors";
 
-async function fetchGame(gameId: string) {
+async function fetchGame(gameId: string): Promise<Game> {
 	const request = getMockGameRequest(gameId);
 
-	return axios
-		.get(request)
-		.then(async (response) => {
-			const gamesData = response.data;
-			return gamesData;
-		})
-		.catch((error) => {
-			console.error(error);
-			return {} as Game;
-		});
+	return axios.get(request).then((response) => {
+		return response.data;
+	});
 }
 
 function GamePageView() {
 	const { gameId } = useLocalSearchParams<{ gameId: string }>();
+	if (!gameId) return <Text>No game id provided.</Text>;
 
-	const [game, setGame] = useState<Game>();
+	const { data, isLoading, isSuccess, isError, error } = useQuery<Game>({
+		queryKey: ["singleGame"],
+		queryFn: () => fetchGame(gameId),
+	});
 
-	useEffect(() => {
-		fetchGame(gameId!).then(setGame);
-	}, []);
+	if (isLoading) return <Text>Loading game id: {gameId}...</Text>;
+	if (isError) {
+		console.error(error);
+		return (
+			<Text style={{ color: "red" }}>Failed loading game id: {gameId}!</Text>
+		);
+	}
+	if (isSuccess) {
+		if (!data)
+			return (
+				<Text style={{ color: "red" }}>No such game with id:{gameId}!</Text>
+			);
 
-	return (
-		<View>
-			<Text>Game: {game?.name}</Text>
-		</View>
-	);
+		return (
+			<View style={{ height: "100%", backgroundColor: colors.background }}>
+				<GameResultView
+					gameId={data.id}
+					title={data.name}
+					imageUri={data.background_image}
+				/>
+			</View>
+		);
+	}
 }
 
 export default GamePageView;
