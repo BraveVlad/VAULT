46b1d9d65c4e1758ed597e9cef8cd621853eacc4
BraveVlad <giladvladpinker@gmail.com>
2024-05-27 23:50:05 +0300

    front:SearchScreen: implemented react query to fetch games list and handle render based on fetch status.

--- vault/app/(tabs)/search.tsx
+++ vault/app/(tabs)/search.tsx
@@ -1,49 +1,73 @@
-import {
-	RAWG_API_KEY,
-	getGamesListRequest,
-	getMockGamesListRequest,
-} from "@/constants/Api";
+import { getMockGamesListRequest } from "@/constants/Api";
 import { mainStyles } from "@/constants/Styles";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	Pressable,
+	ScrollView,
+} from "react-native";
 import axios from "axios";
-import { ApiResponse as GamesResponse, Game } from "@/models/Game.Model";
+import {
+	ApiResponse as GamesResponse,
+	Game,
+	Games,
+	ApiResponse,
+} from "@/models/Game.Model";
 import GameslistView from "@/components/search-screen/GameslistView";
+import { useQuery } from "@tanstack/react-query";
+import refreshIcon from "@/assets/images/replay.png";
 
 async function fetchGames() {
-	// const request = getGamesListRequest(10);
 	const request = getMockGamesListRequest();
 
-	return axios
-		.get(request)
-		.then(async (response) => {
-			const gamesData = response.data;
-			// console.log(gamesData);
-			return gamesData;
-		})
-		.catch((error) => {
-			console.error(error);
-			return {} as GamesResponse;
-		});
+	return axios.get(request).then(async (response) => response.data);
 }
 
 export default function SearchScreen() {
-	const [gamesList, setGamesList] = useState<GamesResponse>();
+	const { data, isLoading, isSuccess, isError, error, refetch } =
+		useQuery<ApiResponse>({
+			queryKey: ["allGames"],
+			queryFn: fetchGames,
+		});
 
-	useEffect(() => {
-		//mount
-		fetchGames().then(setGamesList);
+	const refreshList = () => {
+		refetch();
+	};
 
-		return () => {
-			//unmount
-		};
-	}, []);
 	return (
 		<View style={mainStyles.Screen}>
-			<Text style={mainStyles.Text}>Total Games: {gamesList?.count}</Text>
-			<GameslistView gamesList={gamesList?.results!} />
+			{isLoading && <Text style={styles.loadingMessage}>loading games...</Text>}
+			{isError && (
+				<View>
+					<Text style={styles.errorMessage}>
+						Failed fetching games from server.
+					</Text>
+					<Pressable onPress={refreshList}>
+						<Image source={refreshIcon} />
+					</Pressable>
+					<ScrollView>
+						<Text style={styles.errorMessage}>{error.message}</Text>
+					</ScrollView>
+				</View>
+			)}
+			{isSuccess && (
+				<View>
+					<Text style={mainStyles.Text}>Total Games: {data.count}</Text>
+					<GameslistView gamesList={data.results} />
+				</View>
+			)}
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	errorMessage: {
+		color: "red",
+	},
+	loadingMessage: {
+		color: "white",
+	},
+});
