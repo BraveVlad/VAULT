67d6b73348586bb8a4e71051bb58584c2fc590ab
BraveVlad <giladvladpinker@gmail.com>
2024-05-29 01:05:39 +0300

    front:SearchScreen: relocated games list fetching query inside GamesListView component for better data locality.

--- vault/app/(tabs)/search.tsx
+++ vault/app/(tabs)/search.tsx
@@ -1,64 +1,12 @@
-import { getMockGamesListRequest } from "@/constants/Api";
 import { mainStyles } from "@/constants/Styles";
-import {
-	StyleSheet,
-	Text,
-	View,
-	Image,
-	Pressable,
-	ScrollView,
-} from "react-native";
-import axios from "axios";
-import {
-	ApiResponse as GamesResponse,
-	Game,
-	Games,
-	ApiResponse,
-} from "@/models/Game.Model";
+import { View } from "react-native";
+
 import GameslistView from "@/components/search-screen/GameslistView";
-import { useQuery } from "@tanstack/react-query";
-import NetworkErrorView from "@/components/search-screen/NetworkErrorView";
-
-async function fetchGames() {
-	const request = getMockGamesListRequest();
-
-	return axios.get(request).then(async (response) => response.data);
-}
 
 export default function SearchScreen() {
-	const { data, isLoading, isSuccess, isError, error, refetch } =
-		useQuery<ApiResponse>({
-			queryKey: ["allGames"],
-			queryFn: fetchGames,
-		});
-
-	const refreshList = () => {
-		refetch();
-	};
-
 	return (
 		<View style={mainStyles.Screen}>
-			{isLoading && <Text style={styles.loadingMessage}>loading games...</Text>}
-			{isError && (
-				<NetworkErrorView
-					clientErrorMessage="Couldn't load games."
-					debugError={error.message}
-					isShowDebugError={true}
-					onRefresh={refreshList}
-				/>
-			)}
-			{isSuccess && (
-				<View>
-					<Text style={mainStyles.Text}>Total Games: {data.count}</Text>
-					<GameslistView gamesList={data.results} />
-				</View>
-			)}
+			<GameslistView />
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	loadingMessage: {
-		color: "white",
-	},
-});
--- vault/components/search-screen/GameslistView.tsx
+++ vault/components/search-screen/GameslistView.tsx
@@ -10,31 +10,58 @@ import {
 import React from "react";
 import { Game, Games } from "@/models/Game.Model";
 import GameResultView from "@/components/search-screen/GameResultView";
-import vaultIcon from "@/assets/images/vault.png";
 import { spacingSizes } from "@/constants/Sizes";
+import { getMockGamesListRequest } from "@/constants/Api";
+import { mainStyles } from "@/constants/Styles";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import NetworkErrorView from "./NetworkErrorView";
+import { ApiResponse as GamesResponse } from "@/models/Game.Model";
 
-type GameslistViewProps = {
-	gamesList: Games;
-};
+async function fetchGames() {
+	const request = getMockGamesListRequest();
+	return axios.get(request).then(async (response) => response.data);
+}
 
-function GameslistView({ gamesList }: GameslistViewProps) {
+function GameslistView() {
+	const { data, isLoading, isSuccess, isError, error, refetch } =
+		useQuery<GamesResponse>({
+			queryKey: ["allGames"],
+			queryFn: fetchGames,
+		});
+
+	const refreshList = () => {
+		refetch();
+	};
+
 	return (
 		<>
-			<FlatList
-				numColumns={2}
-				style={styles.GamesList}
-				data={gamesList}
-				keyExtractor={(item: Game, index: number) => `${item.id}`}
-				renderItem={({ item, index, separators }: ListRenderItemInfo<Game>) => {
-					return (
-						<GameResultView
-							gameId={item.id}
-							title={item.name}
-							imageUri={item.background_image}
-						/>
-					);
-				}}
-			/>
+			{isLoading && <Text style={styles.loadingMessage}>loading games...</Text>}
+			{isError && (
+				<NetworkErrorView
+					clientErrorMessage="Couldn't load games."
+					debugError={error.message}
+					isShowDebugError={true}
+					onRefresh={refreshList}
+				/>
+			)}
+			{isSuccess && (
+				<FlatList
+					numColumns={2}
+					style={styles.GamesList}
+					data={data.results}
+					keyExtractor={(item: Game) => `${item.id}`}
+					renderItem={({ item }: ListRenderItemInfo<Game>) => {
+						return (
+							<GameResultView
+								gameId={item.id}
+								title={item.name}
+								imageUri={item.background_image}
+							/>
+						);
+					}}
+				/>
+			)}
 		</>
 	);
 }
@@ -47,5 +74,8 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		width: "100%",
 		flexWrap: "wrap",
+	},
+	loadingMessage: {
+		color: "white",
 	},
 });
