131e4676d93461ed73db5aa6b6020af11022c365
BraveVlad <giladvladpinker@gmail.com>
2024-05-29 01:03:33 +0300

    front:root: added react query devtools

--- vault/app/_layout.tsx
+++ vault/app/_layout.tsx
@@ -2,7 +2,7 @@ import MainContainer from "@/components/layout/MainContainer";
 import TabsBar from "@/components/layout/TabsBar";
 import TopBar from "@/components/layout/TopBar";
 import { useMainFonts } from "@/hooks/useFonts";
-import { NavigationContainer } from "@react-navigation/native";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot, Stack } from "expo-router";
 import { Text } from "react-native";
@@ -20,6 +20,7 @@ export default function RootLayout() {
 			<QueryClientProvider client={queryClient}>
 				<TopBar />
 				<Slot />
+				<ReactQueryDevtools initialIsOpen={true} />
 			</QueryClientProvider>
 		</>
 	);
