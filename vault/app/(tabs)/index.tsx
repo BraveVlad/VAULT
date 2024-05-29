a02cddaedceaa6586d3c027fd28f30fb16ece72c
BraveVlad <giladvladpinker@gmail.com>
2024-05-29 07:12:32 +0300

    front:DiscoverScreen: app's new index is DiscoverScreen which shows the GamesSwipeCardsView component

--- vault/app/(tabs)/index.tsx
+++ vault/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
+import { GamesSwipeCardsView } from "@/components/games/GamesSwipeCardsView";
+import NetworkErrorView from "@/components/games/NetworkErrorView";
 import { mainStyles } from "@/constants/Styles";
 import { Text, View } from "react-native";
 
-export default function App() {
+export default function DiscoverScreen() {
 	return (
 		<View style={mainStyles.Screen}>
-			<Text style={mainStyles.Text}>INDEX</Text>
+			<GamesSwipeCardsView />
 		</View>
 	);
 }
