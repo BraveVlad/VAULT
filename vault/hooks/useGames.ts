3dc8cda078bd4e03b1aaf273e66c68431679e1ce
BraveVlad <giladvladpinker@gmail.com>
2024-05-30 04:16:44 +0300

    front:useGamesHook: fixed response without type

--- vault/hooks/useGames.ts
+++ vault/hooks/useGames.ts
@@ -1,12 +1,12 @@
 import { buildMockGamesListRequest } from "@/constants/Api";
 import axios from "axios";
-import { ApiResponse as GamesResponse } from "@/models/Game.Model";
+import { ApiResponse, ApiResponse as GamesResponse } from "@/models/Game.Model";
 import { useQuery } from "@tanstack/react-query";
 
 async function fetchGames() {
 	const request = buildMockGamesListRequest();
 	const result = await axios.get(request);
-	return result.data;
+	return result.data as ApiResponse;
 }
 
 export default function useGames() {
