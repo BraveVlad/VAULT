b984fd5a36b822f87eeeb0c8cc98befa4a1038a8
BraveVlad <giladvladpinker@gmail.com>
2024-05-30 04:17:49 +0300

    front:VaultScreen: renders user's games using flat list and fetched games from useGames hook

--- vault/app/(tabs)/vault.tsx
+++ vault/app/(tabs)/vault.tsx
@@ -1,15 +1,44 @@
+import GameResultView from "@/components/games/GamesListItemView";
 import NetworkErrorView from "@/components/games/NetworkErrorView";
 import { mainStyles } from "@/constants/Styles";
+import useGames from "@/hooks/useGames";
 import useUser from "@/hooks/useUser";
-import { StyleSheet, Text, View } from "react-native";
+import { Game } from "@/models/Game.Model";
+import { VaultGame } from "@/models/User.Model";
+import {
+	FlatList,
+	ListRenderItemInfo,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 
 export default function VaultScreen() {
 	const { userQuery, isVaultEmpty } = useUser("Dracula");
+	const { gamesQuery } = useGames();
 
 	function handleRefresh() {
 		userQuery.refetch();
 	}
 
+	function convertGameIdToGame(gameId: number) {
+		const game = gamesQuery.data?.results.find((gameInfo) => {
+			return gameInfo.id === gameId;
+		});
+
+		if (!game) return <Text key={gameId}>Couldn't find game {gameId}</Text>;
+
+		return (
+			<view style={styles.vaultItem}>
+				<GameResultView
+					key={game.id}
+					gameId={game.id}
+					title={game.name}
+					imageUri={game.background_image}
+				/>
+			</view>
+		);
+	}
 	return (
 		<View style={mainStyles.Screen}>
 			{userQuery.isLoading && <Text>Loading user...</Text>}
@@ -25,14 +54,31 @@ export default function VaultScreen() {
 				<>
 					<Text>User: {userQuery.data.username} vault:</Text>
 					{isVaultEmpty && <Text>Vault is empty. Add games!</Text>}
-					{!isVaultEmpty &&
-						userQuery.data.vault.map((vaultGame) => (
-							<Text key={vaultGame.gameId}>• {vaultGame.gameId}</Text>
-						))}
+
+					<View>
+						{!isVaultEmpty && (
+							<FlatList
+								numColumns={4}
+								data={userQuery.data.vault}
+								keyExtractor={(item: VaultGame) => `${item.gameId}`}
+								renderItem={({ item }: ListRenderItemInfo<VaultGame>) => {
+									return convertGameIdToGame(item.gameId);
+								}}
+							/>
+						)}
+					</View>
 				</>
 			)}
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	vaultList: {
+		width: "100%",
+		flexWrap: "wrap",
+	},
+	vaultItem: {
+		height: "50%",
+	},
+});
