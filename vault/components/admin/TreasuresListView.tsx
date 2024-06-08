f8860a7d9c6cd9a7513e79a228b4739498bfe04b
BraveVlad <giladvladpinker@gmail.com>
2024-06-08 04:54:33 +0300

    App: TreasureListView: implemented basic fetch request to get all treasures from API

--- vault/components/admin/TreasuresListView.tsx
+++ vault/components/admin/TreasuresListView.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { View, Text } from "react-native";
+
+async function fetchAllTreasuresAsync() {
+	const request = "http://127.0.0.1:3000/treasures";
+	const result = await axios.get(request);
+	return result.data;
+}
 
 export default function TreasuresListView() {
 	return (
