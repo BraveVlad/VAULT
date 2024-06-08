3d1ba24142eb034d1c22489024114fe6222af4a3
BraveVlad <giladvladpinker@gmail.com>
2024-06-08 19:22:46 +0300

    App/AdminRout/Layout: removed QueryClient call. root already wraps the whole app with it.

--- vault/app/admin/_layout.tsx
+++ vault/app/admin/_layout.tsx
@@ -1,14 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 
-export default function RootLayout() {
-	const queryClient = new QueryClient();
-
-	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<Slot />
-			</QueryClientProvider>
-		</>
-	);
+export default function AdminLayout() {
+	return <Slot />;
 }
