914dfd5809774552782362eb6e38a587d8d3dff8
BraveVlad <giladvladpinker@gmail.com>
2024-06-08 05:49:07 +0300

    App: TreasureListView: now implements FlatList with TreasureItem rendered for each treasure fetched.

--- vault/components/admin/TreasuresListView.tsx
+++ vault/components/admin/TreasuresListView.tsx
@@ -1,7 +1,16 @@
-import { Treasures } from "@/models/Treasure.Model";
+import useGame from "@/hooks/useGame";
+import { Treasure, Treasures } from "@/models/Treasure.Model";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { View, Text } from "react-native";
+import {
+	View,
+	Text,
+	FlatList,
+	Pressable,
+	ListRenderItemInfo,
+	StyleSheet,
+	Image,
+} from "react-native";
 
 async function fetchAllTreasuresAsync() {
 	const request = "http://127.0.0.1:3000/treasures";
@@ -15,12 +24,58 @@ export default function TreasuresListView() {
 		queryFn: fetchAllTreasuresAsync,
 	});
 
+	function renderTreasureItem({ item }: ListRenderItemInfo<Treasure>) {
+		return <TreasureListViewItem treasure={item} />;
+	}
+
 	return (
 		<View>
 			{allTreasuresQuery.isLoading && <Text>Loading all treasures...</Text>}
 			{allTreasuresQuery.isSuccess && (
-				<Text>{JSON.stringify(allTreasuresQuery.data[0])}</Text>
+				<FlatList
+					style={styles.TreasureList}
+					data={allTreasuresQuery.data}
+					keyExtractor={(treasure) => treasure.id}
+					renderItem={renderTreasureItem}
+				/>
 			)}
 		</View>
 	);
 }
+
+type TreasureListViewItemProps = {
+	treasure: Treasure;
+};
+
+function TreasureListViewItem({ treasure }: TreasureListViewItemProps) {
+	const lootGame = useGame(treasure.loot.relatedGameId.toString());
+
+	return (
+		<Pressable>
+			<Text>ID: {treasure.id}</Text>
+			<Text>Loot:</Text>
+			<View style={styles.TreasureLoot__View}>
+				<Image
+					style={styles.TreasureLoot__Image}
+					source={{ uri: lootGame.query.data?.background_image }}
+				/>
+				<Text>{lootGame.query.data?.name}</Text>
+			</View>
+		</Pressable>
+	);
+}
+
+const styles = StyleSheet.create({
+	TreasureList: {
+		height: "25%",
+	},
+	TreasureItem: {},
+	TreasureLoot__View: {
+		flexDirection: "row",
+		gap: 8,
+	},
+	TreasureLoot__Image: {
+		width: 16,
+		aspectRatio: 1,
+	},
+});
