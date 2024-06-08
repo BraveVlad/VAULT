009af26fdc684f35578a3ace44efe8ac5b0e4751
BraveVlad <giladvladpinker@gmail.com>
2024-06-08 03:43:50 +0300

    Server: Treasure API: added endpoint to get nearby treasures based on coordinate and search radius

--- vault-server/src/Treasure.Router.ts
+++ vault-server/src/Treasure.Router.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 import { treasures } from "./data/treasure-data";
+import {
+	DEFAULT_USERS_SEARCH_RADIUS_IN_KM,
+	findTreasuresByDistance,
+} from "./models/Treasure.Model";
 
 export const router = Router();
 
@@ -7,4 +11,31 @@ router.get("/treasures", async (req, res) => {
 	console.log(`All treasyres requested from ip: ${req.ip}`);
 	res.status(200);
 	res.json(treasures);
+});
+
+router.get("/treasures/nearby", (req, res) => {
+	const { longitude, latitude, distance } = req.query;
+
+	if (!longitude || !latitude) {
+		res.status(400);
+		res.send(`Please specify coordinates to search treasures nearby.`);
+	}
+
+	const coordinate = {
+		longitude: parseFloat(longitude as string),
+		latitude: parseFloat(latitude as string),
+	};
+
+	const searchRadius = !distance
+		? DEFAULT_USERS_SEARCH_RADIUS_IN_KM
+		: parseFloat(distance as string);
+
+	const treasuresNearby = findTreasuresByDistance(
+		treasures,
+		coordinate,
+		searchRadius
+	);
+
+	res.status(200);
+	res.json(treasuresNearby);
 });
