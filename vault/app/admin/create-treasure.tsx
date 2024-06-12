9a0a1fb4ccf2a68ac3a528b22804f3382b6ab4ef
BraveVlad <giladvladpinker@gmail.com>
2024-06-12 23:56:45 +0300

    App/Admin/CreateTreasure - + First CreateTreasureScreen init + add navigation to the new CreateTreasureScreen

---
+++ vault/app/admin/create-treasure.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function CreateTreasureScreen() {
+	return <Text>Create New Treasure</Text>;
+}
--- vault/app/admin/index.tsx
+++ vault/app/admin/index.tsx
@@ -1,8 +1,9 @@
 import AdminMapView from "@/components/admin/AdminMapView";
 import TreasuresListView from "@/components/admin/TreasuresListView";
 import { mainStyles } from "@/constants/Styles";
+import { Link } from "expo-router";
 import { useState } from "react";
-import { Platform, View } from "react-native";
+import { Platform, Text, View } from "react-native";
 
 export default function AdminScreen() {
 	const [selectedTreasure, setSelectedTreasure] = useState<
@@ -12,8 +13,13 @@ export default function AdminScreen() {
 	function handleSelectedTreasure(treasureId: string) {
 		setSelectedTreasure(treasureId);
 	}
+
 	return (
 		<View style={[mainStyles.AdminScreen, { gap: 6 }]}>
+			<Link push href="/admin/create-treasure">
+				<Text>New Treasure</Text>
+			</Link>
+
 			<TreasuresListView
 				selectedTreasure={selectedTreasure}
 				onSelectedTreasure={handleSelectedTreasure}
